fc50fec80050168c2dae98e6688a00cbeaa3a94e
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-04 07:45:20 +0530

    Admin functionality to user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signIn = async (req,res,next) =>{
     const validPassword = bcryptjs.compareSync(password,validUser.password)
     if(!validPassword) return next(errorHandler(201,'Invalid password'))
   
-    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    const token = jwt.sign({id:validUser._id,isAdmin:validUser.isAdmin},process.env.JWT_SECRET)
     const {password:pass,...rest} = validUser._doc
     res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
     

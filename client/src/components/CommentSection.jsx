b4590a5f8b36e9956562f47128cd96343808c4b0
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-08 12:16:57 +0530

    Handle like in post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,128 +1,156 @@
 import { Alert, Button, Textarea } from 'flowbite-react'
 import { useEffect, useState } from 'react'
-import {useSelector} from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 import Comment from './Comment'
 
-export default function CommentSection({postId}) {
+export default function CommentSection({ postId }) {
 
-    const {currentUser} = useSelector(state=>state.user)
-    const [comment,setComment] = useState('')
-    const[commentError,setCommentError] = useState(false)
-    const[showComment,setShowComment] = useState([])
+    const { currentUser } = useSelector(state => state.user)
+    const [comment, setComment] = useState('')
+    const [commentError, setCommentError] = useState(false)
+    const [showComment, setShowComment] = useState([])
+    const navigate = useNavigate()
 
-    useEffect(()=>{
-        const fetchComments = async()=>{
-       
-        try{
-            const res = await fetch(`/api/comment/getPostComments/${postId}`)
-            const data = await res.json()
-            setShowComment(data)
+    useEffect(() => {
+        const fetchComments = async () => {
+            try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}`)
+                const data = await res.json()
+                setShowComment(data)
+            } catch (error) {
+                console.log(error.message)
+            }
         }
-        catch(error){
-            console.log(error.message);
-        }
-       }
         fetchComments()
-    },[postId])
+    }, [postId])
 
-    const handleSubmit = async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        if(comment.length > 200){
+        if (comment.length > 200) {
             return
         }
 
-       try {
-        setCommentError(null)
-
-        const res = await fetch('/api/comment/create',{
-            method:"POST",
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                postId,content:comment,userId:currentUser._id
-            })
-        })
-
-        const data = await res.json()
-
-        if(res.ok){
-            setComment('')
+        try {
             setCommentError(null)
-            setShowComment([data,...showComment])
-        }
-       } 
 
-       catch (error) {
-        setCommentError(error.message) 
-       }
+            const res = await fetch('/api/comment/create', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    postId,
+                    content: comment,
+                    userId: currentUser._id
+                })
+            })
+
+            const data = await res.json()
+
+            if (res.ok) {
+                setComment('')
+                setCommentError(null)
+                setShowComment([data, ...showComment])
+            }
+        } catch (error) {
+            setCommentError(error.message)
+        }
     }
 
+    const handleLike = async (commentId) => {
+        try {
+          if (!currentUser) {
+            navigate('/sign-in');
+            return;
+          }
+          const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+            method: 'PUT',
+          });
+          if (res.ok) {
+            const data = await res.json();
+            setShowComment(
+              showComment.map((comment) =>
+                comment._id === commentId
+                  ? {
+                      ...comment,
+                      likes: data.likes,
+                      numberOfLikes: data.likes.length,
+                    }
+                  : comment
+              )
+            );
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+
     return (
-    <div className='max-w-2xl mx-auto w-full p-3'>
-        {
-            currentUser ? 
-            (
-                <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
-                    <p>Signed in as:</p>
-                    <img src={currentUser.avatar} alt="" className='h-5 w-5 rounded-full object-cover'/>
-                    <Link to='/dashboard?tab=profile' className='text-xs text-cyan-600 hover:underline'>
-                    @ {currentUser.username}
-                    </Link>
-                </div>
-            )
-            :
-            (
-                <div className='text-sm text-teal-500 my-5 flex gap-3'>
-                    You must be signed in to comment
-                    <Link to='/sign-in' className='text-blue-500 hover:underline'>
-                        Sign In
-                    </Link>
-                </div>
-            )}
-                {
-                    currentUser &&(
+        <div className='max-w-2xl mx-auto w-full p-3'>
+            {
+                currentUser ?
+                    (
+                        <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
+                            <p>Signed in as:</p>
+                            <img src={currentUser.avatar} alt="" className='h-5 w-5 rounded-full object-cover' />
+                            <Link to='/dashboard?tab=profile' className='text-xs text-cyan-600 hover:underline'>
+                                @ {currentUser.username}
+                            </Link>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className='text-sm text-teal-500 my-5 flex gap-3'>
+                            You must be signed in to comment
+                            <Link to='/sign-in' className='text-blue-500 hover:underline'>
+                                Sign In
+                            </Link>
+                        </div>
+                    )}
+            {
+                currentUser && (
                     <form className='border border-teal-500 rounded-md p-3' onSubmit={handleSubmit}>
-                        <Textarea 
-                        placeholder='Add a comment..'
-                        rows='3'
-                        maxLength='200'
-                        onChange={(e)=>setComment(e.target.value)}
-                        value={comment}
+                        <Textarea
+                            placeholder='Add a comment..'
+                            rows='3'
+                            maxLength='200'
+                            onChange={(e) => setComment(e.target.value)}
+                            value={comment}
                         />
                         <div className='flex justify-between items-center mt-3'>
                             <p className='text-gray-500 text-sm'>{200 - comment.length} characters left remaining</p>
                             <Button outline gradientDuoTone='purpleToBlue' type='submit' size='sm'>Submit</Button>
-                        </div>     
+                        </div>
                         {
-                        commentError && 
-                        <Alert color='failure' className='mt-5'>
-                            {commentError}
-                        </Alert>
-                        }                   
-                    </form> 
-                    )}
-                    {
-                        showComment.length === 0 ?
-                        (
-                            <p className='text-sm my-5'>No comments yet</p>
-                        )
-                        :
-                        (
-                            <>
+                            commentError &&
+                            <Alert color='failure' className='mt-5'>
+                                {commentError}
+                            </Alert>
+                        }
+                    </form>
+                )
+            }
+            {
+                showComment.length === 0 ?
+                    (
+                        <p className='text-sm my-5'>No comments yet</p>
+                    )
+                    :
+                    (
+                        <>
                             <div className='text-sm my-5 items-center flex gap-1'>
                                 <p>Comments</p>
                                 <div className='border border-gray-400 py-1 px-2 rounded-sm'>
                                     <p>{showComment.length}</p>
                                 </div>
                             </div>
-                            {showComment.map((comment)=>(
-                                <Comment key={comment._id} comment={comment}/>
+                            {showComment.map((comment) => (
+                                <Comment key={comment._id} comment={comment} onLike={handleLike} />
                             ))}
-                            </>
-                        )
-                    }
-    </div>
-  )
+                        </>
+                    )
+            }
+        </div>
+    )
 }

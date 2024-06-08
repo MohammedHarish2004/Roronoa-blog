b8fd6ab56f72b026ce288419fbfa4dfe2cc87c71
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-08 09:49:53 +0530

    Add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
 import postRouter from './routes/post.route.js'
+import commentRouter from './routes/comment.route.js'
 
 import cookieParser from 'cookie-parser'
 import path from 'path'
@@ -30,6 +31,7 @@ app.use(cookieParser())
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.use('/api/post',postRouter)
+app.use('/api/comment',commentRouter)
 
 app.use(express.static(path.join(__dirname,'/client/dist')));
 
